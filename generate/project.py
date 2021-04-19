ccbd2cacc77d02a0d805f5e82f06f95bdf12dc7e
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-19 21:15:51 +0530

    Project setup

---
+++ generate/project.py
@@ -0,0 +1,6 @@
+
+class Project(object):
+    def __init__(self, project_name, project_files, project_folders):
+        self.name = project_name
+        self.files = project_files
+        self.folders = project_folders
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+DEPENDENCIES = []
+
+
+setuptools.setup(
+    name="woolang-project-generator",  # Replace with your own username
+    version="3.0.0",
+    author="P Pranav Baburaj",
+    author_email="code-roller@googlegroups.com",
+    description=
+    "The project generator for woolang",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woo-lang/woolang-project-generator",
+    packages=setuptools.find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=DEPENDENCIES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
